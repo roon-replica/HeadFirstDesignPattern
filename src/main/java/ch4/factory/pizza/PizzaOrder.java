0f944d3072fd56cea47fcb2ef13d02ee45d5efa6
leemr <think-roon@naver.com>
2022-08-21 19:40:47 +0900

    [4. facotry] simple factory를 이용하여 변경되는 부분 분리 (아직 팩토리 패턴은 아니라고 함..!)

--- src/main/java/ch4/factory/pizza/PizzaOrder.java
+++ src/main/java/ch4/factory/pizza/PizzaOrder.java
@@ -4,22 +4,8 @@ public class PizzaOrder {
     OrderState state;
 
     public Pizza orderPizza(Pizza.PizzaType pizzaType) throws PizzaTypeNotFoundException {
-        Pizza pizza;
 
-        // 이 코드는 변경에 닫혀있지 않다고 함
-        // 메뉴를 추가하거나 제거하면 if문을 추가하거나 제거해야 함...
-        if (pizzaType.equals(Pizza.PizzaType.CHEESE)) {
-            pizza = new CheesePizza();
-            pizza.pizzaType = Pizza.PizzaType.CHEESE;   //todo: 피자의 타입을 여기서 정하는게 적절할지? 말은 되는거 같은데
-        } else if (pizzaType.equals(Pizza.PizzaType.POTATO)) {
-            pizza = new PotatoPizza();
-            pizza.pizzaType = Pizza.PizzaType.POTATO;
-        } else if (pizzaType.equals(Pizza.PizzaType.PEPPERONI)) {
-            pizza = new PepperoniPizza();
-            pizza.pizzaType = Pizza.PizzaType.PEPPERONI;
-        } else {
-            throw new PizzaTypeNotFoundException("pizza type with " + pizzaType + "not exist");  //never
-        }
+        var pizza = SimplePizzaFactory.createPizza(pizzaType);
 
         // 이부분은 거의 변경되지 않는 부분!
         pizza.prepare(this);
---
+++ src/main/java/ch4/factory/pizza/SimplePizzaFactory.java
@@ -0,0 +1,31 @@
+package ch4.factory.pizza;
+
+// 피자 객체 생성만 담당!(Single Responsibility)
+// 피자 객체 생성 작업을 캡슐화해 놓음으로써 피자 생성 로직을 수정할 때, 여기저기서 수정하지 않고 여기만 수정하면 됨!
+public class SimplePizzaFactory {
+
+    // static으로 선언하면 팩토리의 인스턴스를 만들지 않아도 된다는 장점이 있음
+    // 대신 서브클래스를 만들어서 오버라이드는 불가..!
+    public static Pizza createPizza(Pizza.PizzaType pizzaType) {
+        Pizza pizza = null;
+
+        if (pizzaType.equals(Pizza.PizzaType.CHEESE)) {
+            pizza = new CheesePizza();
+            pizza.pizzaType = Pizza.PizzaType.CHEESE;   //todo: 피자의 타입을 여기서 정하는게 적절할지? 말은 되는거 같은데
+        } else if (pizzaType.equals(Pizza.PizzaType.POTATO)) {
+            pizza = new PotatoPizza();
+            pizza.pizzaType = Pizza.PizzaType.POTATO;
+        } else if (pizzaType.equals(Pizza.PizzaType.PEPPERONI)) {
+            pizza = new PepperoniPizza();
+            pizza.pizzaType = Pizza.PizzaType.PEPPERONI;
+        } else {
+            try {
+                throw new PizzaTypeNotFoundException("pizza type not exist"); //never
+            } catch (PizzaTypeNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return pizza;
+    }
+}
