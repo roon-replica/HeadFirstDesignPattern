b230aeae26146d728fca5f60e84e4934a3e8499b
leemr <think-roon@naver.com>
2022-08-21 20:25:59 +0900

    [4. facotry] 데모 실행 클래스

--- src/main/java/ch4/factory/pizzaEx/Main.java
+++ src/main/java/ch4/factory/pizzaEx/Main.java
@@ -1,13 +1,22 @@
 package ch4.factory.pizzaEx;
 
+import ch4.factory.pizzaEx.order.chicago.ChicagoPizzaOrder;
 import ch4.factory.pizzaEx.order.newyork.NewYorkPizzaOrder;
 import ch4.factory.pizzaEx.pizza.PizzaType;
 
 public class Main {
     public static void main(String[] args) throws PizzaTypeNotFoundException {
-        var pizzaOrder = new NewYorkPizzaOrder();
-        var pizza = pizzaOrder.orderPizza(PizzaType.CHEESE);
+        var myPizzaOrder = new NewYorkPizzaOrder();
+        var myPizza = myPizzaOrder.orderPizza(PizzaType.CHEESE);
 
-        System.out.println(pizza + " " + pizzaOrder);
+        System.out.println(myPizza);
+        System.out.println(myPizzaOrder);
+
+
+        var hisPizzaOrder = new ChicagoPizzaOrder();
+        var hisPizza = hisPizzaOrder.orderPizza(PizzaType.POTATO);
+
+        System.out.println(hisPizza);
+        System.out.println(hisPizzaOrder);
     }
 }
