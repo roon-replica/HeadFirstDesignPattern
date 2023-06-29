bac7a00da15b2f9d00fc0bc8b0492611c9ca79e8
roon <think-roon@naver.com>
2023-06-29 22:04:28 +0900

    refactor(6.command): 구체 커맨드 객체를 만들어서 사용하는 대신 동작(함수 객체)를 넘겨주는 방식으로 변경!!
    - Command가 Functional interface라서 execute() 동작만 넘겨주면 됨!!
      근데 이 방법은 Command가 Functional Interface일 때만 사용할 수 있음..
    - 구체 커맨드 객체를 안 만들어도 되는 효과
    - lazy한 계산이 되는 효과도 있지않나? 바로 실행되는게 아니라 동작을 넘겨주니까

--- src/main/java/ch6/command/remote_control/SimpleRemoteControlDemo.java
+++ src/main/java/ch6/command/remote_control/SimpleRemoteControlDemo.java
@@ -1,21 +1,30 @@
 package ch6.command.remote_control;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class SimpleRemoteControlDemo {
     public static void main(String[] args) {
         Light livingRoomLight = new Light("livingRoom");
         Light bedRoomLight = new Light("bedRoom");
         Door garageDoor = new Door("garage");
 
-        Command noCommand = new NoCommand();
         SimpleRemoteControl remoteControl = new SimpleRemoteControl();
 
-        LightOnCommand livingRoomLightOnCommand = new LightOnCommand(livingRoomLight);
-        remoteControl.setCommand(0, livingRoomLightOnCommand, noCommand);
-        remoteControl.pushOnButton(0);
+        Runnable noCommand = () -> System.out.println("[No Command]");
 
-        GarageDoorOpenCommand garageDoorOpenCommand = new GarageDoorOpenCommand(garageDoor);
-        remoteControl.setCommand(1, garageDoorOpenCommand, noCommand);
+        // 커맨드 객체를 따로 안만들고 execute()가 수행할 동작을 넘겨주는 방식!! (함수 객체를 넘겨주는)
+        // 그럼 구체 커맨드 객체를 안만들어도 됨!!
+        remoteControl.setCommand(0, livingRoomLight::on, noCommand::run);
+        remoteControl.pushOnButton(0);
+        System.out.println(livingRoomLight.isOn());
+
+        remoteControl.setCommand(1, bedRoomLight::on, noCommand::run);
+        System.out.println(bedRoomLight.isOn());
+
+        remoteControl.setCommand(1, garageDoor::open, noCommand::run);
         remoteControl.pushOnButton(1);
+        System.out.println(garageDoor.isOpen());
 
         System.out.println(remoteControl);
     }
